8c1dc483d8a7d18934983e1ea16fb038793c507a
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-02-16 20:16:33 -0300

    finishing splash screen

--- app/src/main/java/com/github/cesar1287/pingpongx/SplashActivity.kt
+++ app/src/main/java/com/github/cesar1287/pingpongx/SplashActivity.kt
@@ -2,11 +2,28 @@ package com.github.cesar1287.pingpongx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.github.cesar1287.pingpongx.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
+    private val binding: ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+
+        initAnimation()
     }
+
+    private fun initAnimation() {
+        val anim = AnimationUtils.loadAnimation(this, R.anim.splash)
+        with(binding) {
+            ivSplashLogo.startAnimation(anim)
+            ivSplashName.startAnimation(anim)
+        }
+    }
+
 }
