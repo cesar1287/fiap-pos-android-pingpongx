59663a208d17e9ec31229d0b1f91413260c5ec04
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-02-16 22:05:39 -0300

    sending an intent pt 1

--- app/src/main/java/com/github/cesar1287/pingpongx/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/pingpongx/MainActivity.kt
@@ -2,10 +2,26 @@ package com.github.cesar1287.pingpongx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.cesar1287.pingpongx.PlayerActivity.Companion.INTENT_PLAYER_AWAY_KEY
+import com.github.cesar1287.pingpongx.PlayerActivity.Companion.INTENT_PLAYER_HOME_KEY
+import com.github.cesar1287.pingpongx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val playerHome = intent.getStringExtra(INTENT_PLAYER_HOME_KEY)
+        val playerAway = intent.getStringExtra(INTENT_PLAYER_AWAY_KEY)
+
+        with(binding) {
+            tvMainPlayerHome.text = playerHome
+            tvMainPlayerAway.text = playerAway
+        }
     }
 }
--- app/src/main/java/com/github/cesar1287/pingpongx/PlayerActivity.kt
+++ app/src/main/java/com/github/cesar1287/pingpongx/PlayerActivity.kt
@@ -1,5 +1,6 @@
 package com.github.cesar1287.pingpongx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.cesar1287.pingpongx.databinding.ActivityPlayerBinding
@@ -13,5 +14,19 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        with(binding) {
+            btPlayerStartGame.setOnClickListener {
+                val intent = Intent(this@PlayerActivity, MainActivity::class.java)
+                intent.putExtra(INTENT_PLAYER_HOME_KEY, etPlayerHome.text.toString())
+                intent.putExtra(INTENT_PLAYER_AWAY_KEY, etPlayerAway.text.toString())
+                startActivity(intent)
+            }
+        }
+    }
+
+    companion object {
+        const val INTENT_PLAYER_HOME_KEY = "playerHome"
+        const val INTENT_PLAYER_AWAY_KEY = "playerAway"
     }
 }
